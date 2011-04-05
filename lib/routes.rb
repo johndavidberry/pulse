6249e64b79961526abfbcb211840408cf0a5edfa
John Berry <jberry@twelvetones.(none)>
2011-04-04 23:31:54 -0700

    Added support for Rails 3.0.0 routes.

--- lib/routes.rb
+++ lib/routes.rb
@@ -1,9 +1,17 @@
 module Pulse
   module Routes
     def pulse(path)
-      connect path, :controller => 'pulse', :action => 'pulse'
+      if Rails.version >= "3.0.0"
+        match path, :controller => 'pulse', :action => 'pulse'
+      else
+        connect path, :controller => 'pulse', :action => 'pulse'
+      end
     end
   end
 end
 
-ActionController::Routing::RouteSet::Mapper.send :include, Pulse::Routes
+if Rails.version >= "3.0.0"
+  ActionDispatch::Routing::Mapper.send :include, Pulse::Routes
+else
+  ActionController::Routing::RouteSet::Mapper.send :include, Pulse::Routes
+end
